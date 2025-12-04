b6c4f542a48169635379c1139dbed7ef929b1c7b
aryan578765 <aryanp1544@gmail.com>
2025-12-04 11:54:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -315,7 +315,7 @@ def main():
     <style>
     .input-card {
         background-color: white;
-        padding: 20px;
+        padding: 2px;
         border-radius: 10px;
         box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
         margin-bottom: 20px;
